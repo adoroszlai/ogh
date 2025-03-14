37776ef337b9b33889a6c6826f1ccf1d1e877b75
Doroszlai, Attila <adoroszlai@apache.org>
2025-03-14 10:17:57 +0100

    simplify artifact regex

--- ozone.go
+++ ozone.go
@@ -4,19 +4,15 @@ import (
 	"regexp"
 )
 
-var basicRE = regexp.MustCompile(`(?:CI / )?(?:basic|unit) \(([^)]+)\)`)
-var integrationRE = regexp.MustCompile(`(?:CI / )?integration \(([^)]+)\)`)
-var matrixRE = regexp.MustCompile(`(?:CI / )?(\w+) \(([^)]+)\)`)
+var singleRE = regexp.MustCompile(`(?:.* / )*(\w+)`)
+var matrixRE = regexp.MustCompile(`(?:.* / )*(\w+) \(([^)]+)\)`)
 
 func JobToArtifactName(job string) string {
-	if basicRE.MatchString(job) {
-		return basicRE.ReplaceAllString(job, "$1")
-	}
-	if integrationRE.MatchString(job) {
-		return integrationRE.ReplaceAllString(job, "it-$1")
-	}
 	if matrixRE.MatchString(job) {
 		return matrixRE.ReplaceAllString(job, "$1-$2")
+	}
+	if singleRE.MatchString(job) {
+		return singleRE.ReplaceAllString(job, "$1")
 	}
 	return job
 }
